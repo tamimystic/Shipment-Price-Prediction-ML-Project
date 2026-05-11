a390cdc6bafee04a2b4e71ce9ce13cc8e9cd075f
tamimystic <tamimhossainforcoding01@gmail.com>
2026-05-12 00:17:52 +0600

    configure project setup and dependency management

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="tamimystic",
+    author_email="hossainsmtamim@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
