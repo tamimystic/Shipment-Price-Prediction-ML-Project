758ced816bd0d413054beea007c59791ba0a614a
tamimystic <tamimhossainforcoding01@gmail.com>
2026-05-16 12:50:31 +0600

    Custom Exception added for this Project

--- demo.py
+++ demo.py
@@ -1,3 +1,9 @@
 from shipment.logger import logging
+from shipment.exception import CustomException
+import sys
 
-logging.info("demo")
+try:
+    t=1/0
+except Exception as e:
+    logging.info(CustomException(e))
+    raise CustomException(e)
