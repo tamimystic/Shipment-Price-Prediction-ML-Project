51296450ee81c7117323ca5300fdce9d70128295
tamimystic <tamimhossainforcoding01@gmail.com>
2026-05-19 18:26:32 +0600

    after data ingestions where data will be save, this path defined in config_entity.py inside shipment/config folder,, it will same "artifacts/DataIngestionArtifacts/Train or test folder"

--- shipment/constants/__init__.py
+++ shipment/constants/__init__.py
@@ -6,8 +6,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+ARTIFACTS_DIR = os.path.join(os.getcwd(), "artifacts", TIMESTAMP)
 
 MODEL_CONFIG_FILE= "config/model.yaml"
-MODEL_FILE_PATH = "config/schema.yaml"
+SCHEMA_FILE_PATH = "config/schema.yaml"
 
 DB_URL = environ["CONNECTION_URI"]
+TEST_SIZE = 0.2
+
+DB_NAME = environ["DB_NAME"]
+COLLECTION_NAME = environ["COLLECTION_NAME"]
+
+DATA_INGESTION_ARTIFACTS_DIR = "DataIngestionArtifacts"
+DATA_INGESTION_TRAIN_DIR = "Train"
+DATA_INGESTION_TEST_DIR = "Test"
+DATA_INGESTION_TRAIN_FILE_NAME = "train.csv"
+DATA_INGESTION_TEST_FILE_NAME = "test.csv"
